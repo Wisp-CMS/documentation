88bf7a5bd5d30f5017d2dafa9adbe35bdcf7e8a8
Raymond Yeh <ray@geek.sg>
2024-06-20 15:23:20 +0800

    chore: adding redirects

--- next.config.mjs
+++ next.config.mjs
@@ -4,8 +4,17 @@ import withSearch from './src/markdoc/search.mjs'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'md', 'ts', 'tsx'],
-}
 
+  async redirects() {
+    return [
+      {
+        source: '/:path*', // Match all paths
+        destination: 'https://www.wisp.blog/docs', // Redirect to the new site, preserving the path
+        permanent: false, // This makes it a temporary redirect (HTTP status code 307)
+      },
+    ];
+  },
+};
 export default withSearch(
   withMarkdoc({ schemaPath: './src/markdoc' })(nextConfig),
 )
